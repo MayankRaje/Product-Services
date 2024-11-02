c7c8984efa8f80869aa8a7c8b512981f35f8e731
MayankRaje <mayankraje7000@gmail.com>
2024-11-02 16:39:44 +0530

    implemented post and get method

--- src/main/java/org/productservices/productservices/productservices/FakeStoreProductService.java
+++ src/main/java/org/productservices/productservices/productservices/FakeStoreProductService.java
@@ -1,7 +1,7 @@
 package org.productservices.productservices.productservices;
 
+import org.productservices.productservices.dtos.GenericProductDto;
 import org.productservices.productservices.dtos.fakeStoreProductDto;
-import org.productservices.productservices.models.Product;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -12,18 +12,47 @@ public class FakeStoreProductService implements ProductService{
 
     private RestTemplateBuilder restTemplateBuilder;
     private String getProductRequestUrl="https://fakestoreapi.com/products/{id}";
+    private String createProductRequestUrl="https://fakestoreapi.com/products";
 
     public FakeStoreProductService(RestTemplateBuilder restTemplateBuilder){
         this.restTemplateBuilder=restTemplateBuilder;
     }
     @Override
+    public GenericProductDto createProduct(GenericProductDto product){
+        RestTemplate restTemplate=restTemplateBuilder.build();
+        ResponseEntity<GenericProductDto>response=restTemplate.postForEntity(createProductRequestUrl,product,GenericProductDto.class);//if 2 variale put both by comma,,
+        //response.getStatusCode();
+        return response.getBody();
+    }
+    @Override
 //    public Product getProductById(Long id) {
-    public String getProductById(Long id) {
+//    public String getProductById(Long id) {
+//        //return new Product();
+//    public Product getProductById(Long id) {
+//        //return new Product();
+//        RestTemplate restTemplate=restTemplateBuilder.build();
+//        ResponseEntity<fakeStoreProductDto>response=restTemplate.getForEntity(getProductRequestUrl, fakeStoreProductDto.class ,id);//if 2 variale put both by comma,,
+//        //response.getStatusCode();
+//        return "here is product id "+id;
+//        //return null;
+//    }
+
+    public GenericProductDto getProductById(Long id) {
         //return new Product();
         RestTemplate restTemplate=restTemplateBuilder.build();
         ResponseEntity<fakeStoreProductDto>response=restTemplate.getForEntity(getProductRequestUrl, fakeStoreProductDto.class ,id);//if 2 variale put both by comma,,
         //response.getStatusCode();
-        return "here is product id "+id;
+
+        fakeStoreProductDto dto=response.getBody();
+
+        GenericProductDto productDto=new GenericProductDto();
+        productDto.setImage(dto.getImage());
+        productDto.setDescription(dto.getDescription());
+        productDto.setTitle(dto.getTitle());
+        productDto.setPrice(dto.getPrice());
+        //return "here is product id "+id;
         //return null;
+        return productDto;
     }
+
 }
--- src/main/java/org/productservices/productservices/productservices/SelfProductServiceImpl.java
+++ src/main/java/org/productservices/productservices/productservices/SelfProductServiceImpl.java
@@ -1,13 +1,23 @@
 package org.productservices.productservices.productservices;
 
+import org.productservices.productservices.dtos.GenericProductDto;
 import org.productservices.productservices.models.Product;
 import org.springframework.stereotype.Service;
 
 @Service("selfProductServiceImpl")
 public class SelfProductServiceImpl implements ProductService{
     @Override
+    public GenericProductDto createProduct(GenericProductDto product) {
+        return null;
+    }
+
+    @Override
 //    public Product getProductById(Long id) {
-    public String getProductById(Long id) {
+//    public String getProductById(Long id) {
+//        return null;
+//    }
+
+    public GenericProductDto getProductById(Long id) {
         return null;
     }
 }
